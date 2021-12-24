2a0e1a313fcd2abf8753ce2cd70f2310ab926d2b
caichuang <1009825377@qq.com>
2021-12-24 13:28:12 +0800

    修改chat_room2存储用户map为sync.Map

--- chat_room2/controller/api/userController.go
+++ chat_room2/controller/api/userController.go
@@ -34,34 +34,59 @@ func init(){
 		for {
 			select {
 			case client:=<-SendMsgChannel:
-
+				re,_:=json.Marshal(client)
 				if client.Msg.MsgType == ConnectedMessage{
-					for _,v:=range onLineMap{
+
+					onLineMapSafe.Range(func(k, value interface{}) bool {
+						v := value.(*Client)
 						client.Msg.Message ="进入聊天室"
-						re,_:=json.Marshal(client)
+						//re,_:=json.Marshal(client)
 						v.Conn.WriteMessage(1,re)
-					}
+						return true
+					})
+					//for _,v:=range onLineMap{
+					//	client.Msg.Message ="进入聊天室"
+					//	re,_:=json.Marshal(client)
+					//	v.Conn.WriteMessage(1,re)
+					//}
 				} else if client.Msg.MsgType == BroadcastMessage{
-					for _,v:=range onLineMap{
-						re,_:=json.Marshal(client)
-						if v != client {
+
+					onLineMapSafe.Range(func(k, value interface{}) bool {
+						v := value.(*Client)
+						if v !=client {
 							v.Conn.WriteMessage(1,re)
 						}
-					}
+						return true
+					})
+
+					//for _,v:=range onLineMap{
+					//	re,_:=json.Marshal(client)
+					//	if v != client {
+					//		v.Conn.WriteMessage(1,re)
+					//	}
+					//}
 				} else if client.Msg.MsgType == DisconnectedMessage{
-					re,_:=json.Marshal(client)
-					for _,v:=range onLineMap{
-						if v != client {
+
+					onLineMapSafe.Range(func(k, value interface{}) bool {
+						v := value.(*Client)
+						if v !=client {
 							v.Conn.WriteMessage(1,re)
 						}
-					}
+						return true
+					})
+					onLineMapSafe.Delete(client.Addr)
+					//for _,v:=range onLineMap{
+					//	if v != client {
+					//		v.Conn.WriteMessage(1,re)
+					//	}
+					//}
 					//关闭连接
-					client.Conn.Close()
-					var wg sync.WaitGroup
-					wg.Add(1)
-					//删除map中的数据
-					delete(onLineMap,client.Addr)
-					wg.Done()
+					//client.Conn.Close()
+					//var wg sync.WaitGroup
+					//wg.Add(1)
+					////删除map中的数据
+					//
+					//wg.Done()
 				}
 			//case <-time.After(10*time.Second):
 			//	//主动断开
@@ -83,8 +108,11 @@ var upGrader = websocket.Upgrader{
 //定义全局在线用户
 var onLineMap = make(map[string]*Client)
 
-//定义全局的消息推送channel
+//并发安全的map
 
+var onLineMapSafe sync.Map
+
+//定义全局的消息推送channel
 var SendMsgChannel = make(chan *Client)
 
 //定义用户结构体
@@ -120,29 +148,47 @@ func (UserController)Ws(c *gin.Context){
 	//name是 "用户-端口"
 	strsli:=strings.Split(remoteAddr,":")
 	name :="用户"+strsli[1]
-	onLineMap[remoteAddr] = &Client{
+	user := &Client{
 		Name: name,
 		Addr:remoteAddr,
 		Conn: ws,
 		Header: "https://tse1-mm.cn.bing.net/th/id/R-C.efdb268bb841fb60073dbae826bf2b9f?rik=Ufo6V0eAyp3IkQ&riu=http%3a%2f%2fscimg.jianbihuadq.com%2f202009%2f202009162308095.jpg&ehk=thgEdzkXNa5AqjDy3cJ5aAHwMPSGcbOS7CKvuxvNo3w%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1",
 		Addtime: time.Now().Format("15:04"),
+		Msg:message{MsgType: ConnectedMessage},
 	}
+	//添加到map中
+	onLineMapSafe.Store(remoteAddr,user)
+	SendMsgChannel<-user
 	//通知其他用户上线消息
-	onLineMap[remoteAddr].Msg = message{MsgType: ConnectedMessage}
-	SendMsgChannel<-onLineMap[remoteAddr]
+	//onLineMap[remoteAddr].Msg = message{MsgType: ConnectedMessage}
+	//SendMsgChannel<-onLineMap[remoteAddr]
 	for {
 		_,p,err:=ws.ReadMessage()
+
+		value,_:=onLineMapSafe.Load(remoteAddr)
+		v :=value.(*Client)
+
+
 		//用户退出
 		if err != nil {
 			fmt.Println("用户退出!")
-			onLineMap[remoteAddr].Msg = message{MsgType: DisconnectedMessage}
-			SendMsgChannel<-onLineMap[remoteAddr]
+
+			//value,_:=onLineMapSafe.Load(remoteAddr)
+			//v :=value.(*Client)
+			v.Msg = message{MsgType: DisconnectedMessage}
+
+			SendMsgChannel<-v
+
+			//onLineMap[remoteAddr].Msg = message{MsgType: DisconnectedMessage}
+			//SendMsgChannel<-onLineMap[remoteAddr]
 			return
 		}
 
 		//群发用户发送的信息
-		onLineMap[remoteAddr].Msg = message{MsgType: BroadcastMessage,Message:string(p)}
-		SendMsgChannel<-onLineMap[remoteAddr]
+		v.Msg =message{MsgType: BroadcastMessage,Message:string(p)}
+
+		//onLineMap[remoteAddr].Msg = message{MsgType: BroadcastMessage,Message:string(p)}
+		SendMsgChannel<-v
 		if err != nil {
 			fmt.Println("socket写入失败:",err)
 			break
@@ -158,9 +204,16 @@ func (UserController)Ws(c *gin.Context){
 func  (UserController)GetOnlineUser(c *gin.Context){
 	c.Header("Access-Control-Allow-Origin", "*")
 	var onLineSlice []*Client
-	for _,v := range onLineMap{
+
+	onLineMapSafe.Range(func(k, value interface{}) bool {
+		v := value.(*Client)
 		onLineSlice = append(onLineSlice,v)
-	}
+		return true
+	})
+
+	//for _,v := range onLineMap{
+	//	onLineSlice = append(onLineSlice,v)
+	//}
 
 	if len(onLineSlice) ==0 {
 		c.JSON(200, []struct {
